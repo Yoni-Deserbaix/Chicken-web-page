2679d01330c22ab9c5830bb6ec834e69b4dadd56
unknown <yonideserbaix@gmail.com>
2023-12-15 22:03:33 +0100

    add copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-
 function Footer () {
     return (
         <footer>
-            <div>Copyright @ YD-SavvyDev</div>
+            <div>Copyright @ Yoni-Deserbaix</div>
         </footer> 
     )
 }
